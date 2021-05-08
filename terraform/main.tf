aa0c16684c7fefe8f63ddc64742c3bbaa3035ddb
raksit31667 <raksit.m@ku.th>
2021-05-08 20:44:26 +0700

    :building_construction: add Lambda function connected to DynamoDB

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,48 @@ resource "aws_dynamodb_table" "rides-table" {
     Environment = "production"
   }
 }
+
+resource "aws_iam_role" "iam-for-lambda" {
+  name = "lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "iam-policy-for-lambda" {
+  name = "lambda-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid : "SpecificTable",
+        Effect : "Allow",
+        Action : [
+          "dynamodb:*"
+        ],
+        Resource : aws_dynamodb_table.rides-table.arn
+      }
+    ]
+  })
+
+  role = aws_iam_role.iam-for-lambda.id
+}
+
+resource "aws_lambda_function" "wild-rydes-lambda" {
+  filename         = "requestUnicorn.zip"
+  function_name    = "RequestUnicorn"
+  handler          = "requestUnicorn.handler"
+  role             = aws_iam_role.iam-for-lambda.arn
+  runtime          = "nodejs12.x"
+  source_code_hash = filebase64sha256("requestUnicorn.zip")
+}
